b567148b23b947e5ee3cff03a3628e84d80071d8
avincent6 <vincent6@purdue.edu>
2018-07-21 15:28:36 -0700

    send a message

--- src/routes/receive-router.js
+++ src/routes/receive-router.js
@@ -7,15 +7,15 @@ var unassignedUsers;
 router.post('/', async (req, res, next) => {
 
   const { token, text, trigger_id } = req.body;
-  console.log('hey');
-  const sawyer = roundRobin();
+  const assignee = await roundRobin();
+  console.log(assignee);
   const name = 'Sawyer';
   if (token === process.env.SLACK_VERIFICATION_TOKEN) {
     console.log('sendConfirmation');
      axios.post('https://slack.com/api/chat.postMessage', qs.stringify({
        token: process.env.SLACK_AUTH_TOKEN,
-       channel: 'UBMJE3QDQ',
-       text: `Bzz! Bzz! Hey ${name}, you've been assigned an email`,
+       channel: assignee,
+       text: `Bzz! Bzz! Hey, you've been assigned an email`,
        attachments: JSON.stringify([{
         "text": "Subject",
               "fallback": "Sorry you were unable to reply",
@@ -43,19 +43,45 @@ router.post('/', async (req, res, next) => {
 });
 
 // Take in a user list and
-function roundRobin() {
+async function roundRobin() {
   if (unassignedUsers === undefined || unassignedUsers.length == 0) {
     // Reset unassignedUsers!
     const channelID = 'CBUAADA1Y';
     const tokenWithChannel = {
       token: process.env.SLACK_AUTH_TOKEN,
-      channel: channelID
+      channel: 'CBUAADA1Y'
     };
-    unassignedUsers = (await axios.post('https://slack.com/api/channels.info', qs.stringify(tokenWithChannel))).data.channel.members;
+    try {
+      console.log((await axios.post('https://slack.com/api/channels.info', qs.stringify(tokenWithChannel))).data.channel.members);
+      unassignedUsers = (await axios.post('https://slack.com/api/channels.info', qs.stringify(tokenWithChannel))).data.channel.members;
+      return getTheUser();
+    } catch (error) {
+      console.log('error', error);
+    }
+  } else {
+    return getTheUser();
   }
   // Do the Round Robin:
-  return unassignedUsers[Math.floor((Math.random() * unassignedUsers.length) + 1)];
+
 };
+
+function getTheUser() {
+  const user = unassignedUsers[Math.floor((Math.random() * unassignedUsers.length) + 1)];
+  console.log('user', user);
+  // unassignedUsers.remove(user);
+  if (unassignedUsers.length === 1) {
+    unassignedUsers = '';
+    return unassignedUsers[0];
+  }
+  unassignedUsers = unassignedUsers.filter(function(el) {
+    return el !== user;
+  })
+
+  console.log('user: ', user);
+
+  console.log('Unassigned: ', unassignedUsers);
+  return user;
+}
 
 const msg = {
     "text": "Hi friend! Bzz Bzz. You've been assigned an email!",
